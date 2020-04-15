1e998187c9851e7361424e6446d2d9158b2b4827
Zack Driscoll <zacdrisc@uat.edu>
2020-04-15 12:17:25 -0700

    Created PlayerController Script and modified GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Allows use of GameManager varibles/functions outside of the GameManager script
+    public static GameManager instance;
+
+    //Scene number to change
+    public int currentScene = 0;
+
+    //Score showing number of coins collected
+    public int playerScore = 0;
+
+    //Destroy second GameManager if there is one
+    void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            Debug.Log("Warning: A second Game Manager was detected and destroyed.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    //Next 3 functions change the scene
+
+    public void LoadLevel(string levelToLoad)
     {
-        
+        SceneManager.LoadScene(levelToLoad);
+        //Potentially dangerous
+        currentScene++;
+    }
+
+    public void LoadLevel(int indexToLoad)
+    {
+        SceneManager.LoadScene(indexToLoad);
+        currentScene = indexToLoad;
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentScene + 1);
+    }
+
+    //Allows user to quit when they press the escape key
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform tf;
+    private Rigidbody2D rb2d;
+    private SpriteRenderer sr;
+    private Animator animator;
+
+    public float movementSpeed = 5.0f;
+    public float jumpForce = 500.0f;
+    public Transform groundPoint;
+    public bool isGrounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = gameObject.GetComponent<Transform>();
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Controlls Horizontal movement
+        float xMovement = Input.GetAxis("Horizontal") * movementSpeed;
+        rb2d.velocity = new Vector2(xMovement, rb2d.velocity.y);
+        if (xMovement > 0)
+        {
+            sr.flipX = false;
+            //animator.Play("PlayerWalk");
+        }
+        else if (xMovement < 0)
+        {
+            sr.flipX = true;
+            //animator.Play("PlayerWalk");
+        }
+        else
+        {
+            //animator.Play("PlayerIdle");
+        }
+
+        //Detect if the player is on the ground
+        RaycastHit2D hitInfo = Physics2D.Raycast(groundPoint.position, Vector2.down, 0.1f);
+        if (hitInfo.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        //Jump with spacebar
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Debug.Log("Jump");
+            rb2d.AddForce(Vector2.up * jumpForce);
+        }
+
+        //Jump with up arrow key
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        {
+            Debug.Log("Jump");
+            rb2d.AddForce(Vector2.up * jumpForce);
+        }
+
+        //Jump with W key
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            Debug.Log("Jump");
+            rb2d.AddForce(Vector2.up * jumpForce);
+        }
+    }
+}
